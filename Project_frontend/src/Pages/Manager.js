b04787a1db90c2dcdb5d5337b43d794c4d95030c
suriya1776 <suriyamuthukumar1776@gmail.com>
2021-10-05 00:02:18 +0530

    task state

--- Project_frontend/src/Pages/Manager.js
+++ Project_frontend/src/Pages/Manager.js
@@ -2,31 +2,27 @@ import React from 'react'
 import './Manager.css'
 import { useProjectContext } from '../Context/GlobalState'
 
-function Manager() {
+function Manager(props) {
   const { project, task, displayTask } = useProjectContext()
-  console.log(task)
 
-  // const taskHandler = (index1) => {
-  //   console.log(project[index1])
-  // }
-  // () => taskHandler(index)
+  const taskHandler = (index1) => {
+    displayTask()
+  }
+
   return (
     <div>
       <h1 className='form_heading '>Manager page</h1>
-
-      <div>
-        {project.map((project, index) => {
-          return (
-            <div className='project_name'>
-              <h1 key={index}>{project}</h1>
-              <button onClick={displayTask}>CREATE TASK</button>
-              {task.map((task) => {
-                return <h1>{task}</h1>
-              })}
-            </div>
-          )
-        })}
-      </div>
+      {project.map((project, index) => {
+        return (
+          <div className='project_name'>
+            <h1 key={index}>
+              {project}
+              <button onClick={() => taskHandler(index)}>CREATE TASK</button>
+            </h1>
+          </div>
+        )
+      })}
+      <div>{task}</div>
     </div>
   )
 }
